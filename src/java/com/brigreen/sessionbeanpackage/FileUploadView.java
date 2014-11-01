216ee1dac2d7a31b20bed3697e312b47db9ffe02
ryan1025 <ryan1025@vt.edu>
2014-11-01 17:11:47 -0400

    File Upload
    HTML for upload needs to be copy and pasted from prime faces but the
    functionality is there. Pathing is questionable and needs to be sorted
    out.

---
+++ src/java/com/brigreen/sessionbeanpackage/FileUploadView.java
@@ -0,0 +1,69 @@
+/*
+ * Created by Ryan Miller on 2014.11.01  * 
+ * Copyright © 2014 Ryan Miller. All rights reserved. * 
+ */
+package com.brigreen.sessionbeanpackage;
+ 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+ 
+import org.primefaces.model.UploadedFile;
+ 
+@ManagedBean
+public class FileUploadView {
+     
+    private UploadedFile file;
+    private String path = "/var/webapp/uploads/";
+ 
+    public UploadedFile getFile() {
+        return file;
+    }
+ 
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+     
+    public void upload() throws IOException{
+        if(file != null) {
+            System.out.println(path);
+            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+            
+             try {
+             copyFile(file.getFileName(), file.getInputstream());
+             } catch (IOException e) {
+             e.printStackTrace();
+             } 
+        }
+    }
+
+    private void copyFile(String fileName, InputStream inputstream) {
+        // write the inputStream to a FileOutputStream
+        try {
+        OutputStream out = new FileOutputStream(new File(path + fileName));
+
+        int read = 0;
+        byte[] bytes = new byte[1024];
+
+        while ((read = inputstream.read(bytes)) != -1) {
+             out.write(bytes, 0, read);
+         }
+
+        inputstream.close();
+        out.flush();
+        out.close();
+
+        System.out.println("New file created!");
+        } catch (IOException e) {
+        System.out.println(e.getMessage());
+        }
+    }
+    
+    
+}
